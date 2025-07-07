f574566d4495021d0dd3a551fafd61accd0561ff
amrit43 <amritmishra43@gmail.com>
2025-07-07 11:47:17 +0530

    My First Maven Project

---
+++ src/main/java/com/amrit/app/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.amrit.app;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI Demo!");
+    }
+}
---
+++ src/test/java/com/amrit/app/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package com.amrit.app;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void testPrint() {
+        HelloWorld.main(null); // Just run the main method
+    }
+}
