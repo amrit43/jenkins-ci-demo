b5f30c1fa5e50a74f173a8a62e6003d7ea91d723
amrit43 <83704198+amrit43@users.noreply.github.com>
2025-07-07 12:36:07 +0530

    Update Java Code

--- src/main/java/com/amrit/app/HelloWorld.java
+++ src/main/java/com/amrit/app/HelloWorld.java
@@ -3,5 +3,6 @@ package com.amrit.app;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins CI Demo!");
+        System.out.println("Hello from Jenkins CI Demo! [Updated at " + java.time.LocalDateTime.now() + "]");
     }
 }
